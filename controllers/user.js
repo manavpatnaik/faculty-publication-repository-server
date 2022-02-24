6216d4d5646b56df7fd5217f9d677635fabbca26
Manav Patnaik <manavpatnaik@hotmail.com>
2022-02-24 18:13:18 +0530

    Moved user functions into separate controller file

---
+++ controllers/user.js
@@ -0,0 +1,96 @@
+const User = require('../models/User');
+
+// Get all users
+exports.getAllUsers = async (req, res) => {
+	const users = await User.find(req.query);
+	res.status(200).send({
+		success: true,
+		data: users
+	});
+};
+
+// Create single user
+exports.createUser = async (req, res) => {
+	const user = new User(req.body);
+	try {
+		await user.save();
+		res.status(200).send({
+			success: true,
+			message: '',
+			data: user
+		});
+	} catch (err) {
+		res.status(400).send({
+			success: false,
+			message: err.message
+		});
+	}
+};
+
+// Get single user
+exports.getSingleUser = async (req, res) => {
+	const { id } = req.params;
+	try {
+		const user = await User.findById(id);
+		res.send({
+			success: true,
+			message: '',
+			data: user
+		});
+	} catch (err) {
+		res.send({
+			success: false,
+			message: err.message
+		});
+	}
+};
+
+// Delete single user
+exports.deleteUser = async (req, res) => {
+	const { id } = req.params;
+	const user = await User.deleteOne({ _id: id });
+	try {
+		res.status(200).send({
+			success: true,
+			message: '',
+			data: user
+		});
+	} catch (err) {
+		res.status(400).send({
+			success: false,
+			message: err.message
+		});
+	}
+};
+
+// Login user using email and password
+exports.loginUser = async (req, res) => {
+	const { email, password } = req.body;
+
+	if (!email || !password)
+		return res.status(400).send({
+			success: false,
+			message: 'Please provide email and password'
+		});
+
+	const user = await User.findOne({ email });
+	if (!user)
+		return res.status(401).send({
+			success: false,
+			message: 'Invalid Credentials'
+		});
+
+	const isPasswordValid = await user.matchPassword(password);
+	if (!isPasswordValid)
+		return res.status(401).send({
+			success: false,
+			message: 'Invalid Credentials'
+		});
+
+	const token = await user.getSignedJwt();
+
+	res.status(200).send({
+		success: true,
+		data: token
+	});
+};
--- routes/user.js
+++ routes/user.js
@@ -1,94 +1,14 @@
 const router = require('express').Router();
-const User = require('../models/User');
+const { getAllUsers, getSingleUser, createUser, loginUser } = require('../controllers/user');
 
-router.get('/', async (req, res) => {
-	const users = await User.find(req.query);
-	res.status(200).send({
-		success: true,
-		data: users
-	});
-});
+router.get('/', getAllUsers);
 
-router.get('/:id', async (req, res) => {
-	const { id } = req.params;
-	try {
-		const user = await User.findById(id);
-		res.send({
-			success: true,
-			message: '',
-			data: user
-		});
-	} catch (err) {
-		res.send({
-			success: false,
-			message: err.message
-		});
-	}
-});
+router.get('/:id', getSingleUser);
 
-router.post('/', async (req, res) => {
-	const user = new User(req.body);
-	try {
-		await user.save();
-		res.status(200).send({
-			success: true,
-			message: '',
-			data: user
-		});
-	} catch (err) {
-		res.status(400).send({
-			success: false,
-			message: err.message
-		});
-	}
-});
+router.post('/', createUser);
 
-router.delete('/:id', async (req, res) => {
-	const { id } = req.params;
-	const user = await User.deleteOne({ _id: id });
-	try {
-		res.status(200).send({
-			success: true,
-			message: '',
-			data: user
-		});
-	} catch (err) {
-		res.status(400).send({
-			success: false,
-			message: err.message
-		});
-	}
-});
+router.delete('/:id', deleteUser);
 
-router.post('/login', async (req, res) => {
-	const { email, password } = req.body;
-
-	if (!email || !password)
-		return res.status(400).send({
-			success: false,
-			message: 'Please provide email and password'
-		});
-
-	const user = await User.findOne({ email });
-	if (!user)
-		return res.status(401).send({
-			success: false,
-			message: 'Invalid Credentials'
-		});
-
-	const isPasswordValid = await user.matchPassword(password);
-	if (!isPasswordValid)
-		return res.status(401).send({
-			success: false,
-			message: 'Invalid Credentials'
-		});
-
-	const token = await user.getSignedJwt();
-
-	res.status(200).send({
-		success: true,
-		data: token
-	});
-});
+router.post('/login', loginUser);
 
 module.exports = router;
